7c2c73c67ae5cd68ed79c71acd24ac74662b7e82
Adam Krawczyk <kemu.krawczyk@onet.pl>
2024-06-04 21:09:03 +0200

    bug fix

--- zagraj-ze-mna/src/Home/Home.jsx
+++ zagraj-ze-mna/src/Home/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
 
   const navigate = useNavigate();
   useEffect(() => {
-    fetchGames(); //UNCOMMENT THIS IF THERE ARE GAMES WITH PICTURES IN THE DATABASE, OTHERWISE LEAVE COMMENTED
+    fetchGames();
   }, [currentPage]);
   const fetchGames = () => {
     setIsLoading(true);
@@ -36,7 +36,7 @@ function Home() {
       .then((data) => {
         setGames(data.Game);
         setMaxPages(data.Pages);
-
+        console.log("---------------FETCH DO STRONY GŁÓWNEJ---------------");
         console.log(data);
       })
       .catch((error) => {
@@ -61,6 +61,7 @@ function Home() {
   };
   const handleSearch = () => {
     fetchGames();
+    setCurrentPage(0);
   };
   const [games, setGames] = useState([]);
 
